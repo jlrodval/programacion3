7ccc64c83f83631e562624cb84abcb3ff1b06ddd
famendola <famendola@alumnos.exa.unicen.edu.ar>
2023-05-03 08:50:05 -0300

    Se añaden las clases para los servicios del TPE

---
+++ src/tpe/ServicioBFS.java
@@ -0,0 +1,21 @@
+package ProgramacionIII.tpe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ProgramacionIII.tp3.Grafo;
+
+public class ServicioBFS {
+
+	private Grafo<?> grafo;
+	
+	public ServicioBFS(Grafo<?> grafo) {
+		this.grafo = grafo;
+	}
+	
+	public List<Integer> bfsForest() {
+		// Resolver BFS
+		return new ArrayList<>();
+	}
+	
+}
---
+++ src/tpe/ServicioDFS.java
@@ -0,0 +1,21 @@
+package ProgramacionIII.tpe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ProgramacionIII.tp3.Grafo;
+
+public class ServicioDFS {
+
+	private Grafo<?> grafo;
+
+	public ServicioDFS(Grafo<?> grafo) {
+		this.grafo = grafo;
+	}
+	
+	public List<Integer> dfsForest() {
+		// Resolver DFS
+		return new ArrayList<>();
+	}
+
+}
