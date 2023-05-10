218db8560890aeed0e74a7699953e86a078e05ec
jlrodval <jlrodval@gmail.com>
2023-05-10 17:34:14 -0300

    first commit

--- src/tp3/GrafoDirigido.java
+++ src/tp3/GrafoDirigido.java
@@ -1,19 +1,32 @@
 package ProgramacionIII.tp3;
 
 import java.util.Iterator;
+import java.util.List;
 
 public class GrafoDirigido<T> implements Grafo<T> {
 
+	private List<Integer> vertices;
+    private List<Arco<T>> arcos;
+
 	@Override
 	public void agregarVertice(int verticeId) {
-		// TODO Auto-generated method stub
-
-	}
+        if (!contieneVertice(verticeId)) {
+            vertices.add(verticeId);
+        }
+    }
 
 	@Override
 	public void borrarVertice(int verticeId) {
-		// TODO Auto-generated method stub
-
+        //Primero borramos las aristas que tienen al vértice que queremos eliminar
+    	for (int i = 0; i < aristas.size(); i++) {
+        	Arco<T> arco = aristas.get(i);
+			if (arco.getVerticeOrigen() == verticeId || arco.getVerticeDestino() == verticeId) {
+				aristas.remove(i);
+				i--;
+			}
+		}
+    	// Luego borramos el vértice de la lista de vértices
+    	vertices.remove((Integer) verticeId);
 	}
 
 	@Override
@@ -24,62 +37,86 @@ public class GrafoDirigido<T> implements Grafo<T> {
 
 	@Override
 	public void borrarArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
-
+		Arco<T> arco = this.obtenerArco(verticeId1, verticeId2);
+		if (arco != null) {
+			arcos.remove(arco);
+		}
 	}
 
 	@Override
 	public boolean contieneVertice(int verticeId) {
-		// TODO Auto-generated method stub
-		return false;
+		return vertices.contains(verticeId);
 	}
 
 	@Override
 	public boolean existeArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
-		return false;
+		for (Arco<T> arco: arcos){			
+			if ((arco.getVerticeOrigen() == verticeId1 && arco.getVerticeDestino() == verticeId2) ||
+            	(arco.getVerticeOrigen() == verticeId2 && arco.getVerticeDestino() == verticeId1)) {
+            	return true;
+        	}
+    	}
+    	return false;
 	}
 
 	@Override
 	public Arco<T> obtenerArco(int verticeId1, int verticeId2) {
-		// TODO Auto-generated method stub
-		return null;
+		for (Arco<T> arco: arcos){			
+			if (this.existeArco(verticeId1, verticeId2)) {
+            	return arco;
+        	}
+    	}
+    	return null;
 	}
 
 	@Override
 	public int cantidadVertices() {
-		// TODO Auto-generated method stub
-		return 0;
+		return vertices.size();
 	}
 
 	@Override
 	public int cantidadArcos() {
-		// TODO Auto-generated method stub
-		return 0;
+		return arcos.size();
 	}
 
 	@Override
 	public Iterator<Integer> obtenerVertices() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Integer> verticesTmp = new ArrayList<>();
+    	for (Integer vertice : vertices) {
+			verticesTmp.add(vertice);
+    	}
+    	return verticesTmp.iterator();
 	}
 
 	@Override
 	public Iterator<Integer> obtenerAdyacentes(int verticeId) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Arco<T>> adyacentes = new ArrayList<>();
+    	for (Arco<T> arco : arcos) {
+        	if (arco.getVerticeOrigen() == verticeId) {
+            	adyacentes.add(arco.getVerticeDestino());
+        	}
+    	}
+    	return arcosTmp.iterator();
 	}
 
 	@Override
 	public Iterator<Arco<T>> obtenerArcos() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Arco<T>> arcosTmp = new ArrayList<>();
+    	for (Arco<T> arco : arcos) {
+			arcosTmp.add(arco);
+    	}
+    	return arcosTmp.iterator();
 	}
 
 	@Override
 	public Iterator<Arco<T>> obtenerArcos(int verticeId) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Arco<T>> arcosTmp = new ArrayList<>();
+    	for (Arco<T> arco : arcos) {
+        	if ((arco.getVerticeOrigen() == verticeId) || (arco.getVerticeDestino() == verticeId)) {
+            	arcosTmp.add(arco);
+        	}
+    	}
+    	return arcosTmp.iterator();
 	}
 
 }
